b6d2c61038cc7a94414c97b83b814790bc027ad6
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-09-16 01:17:26 +0500

    accardion done

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const accardions = document.querySelectorAll('.accardion_item')
+const accardions = document.querySelectorAll(".accardion_lists");
 
-accardions.forEach(item => {
-    item.addEventListener('click', e =>{
-        item.classList.toggle("active")
-    })
-})
+accardions.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    accardions.forEach((item2) => {
+      if (item2 !== item && item2.classList.contains("active")) {
+        item2.classList.remove("avtive");
+      }
+
+    //   kod-2 AI 
+    
+      if (item2 !== item && item2.classList.contains("active")) {
+        item2.classList.remove("active");
+      }
+    });
+    item.classList.toggle("active");
+  });
+});
+
+
